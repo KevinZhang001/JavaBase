d174aec2b5727e563f7233254a1a22b365e86413
kevin <kevinzhang001@163.com>
2018-01-15 23:04:44 +0800

    Tree map explore

--- src/main/java/com/explore/TreeMapExplore.java
+++ src/main/java/com/explore/TreeMapExplore.java
@@ -1,5 +1,7 @@
 package com.explore;
 
+import java.util.NavigableMap;
+import java.util.Random;
 import java.util.TreeMap;
 
 /**
@@ -7,10 +9,23 @@ import java.util.TreeMap;
  */
 public class TreeMapExplore {
     public static void main(String[] args) {
-
+        TreeMapApiExplore();
     }
 
     private static void TreeMapApiExplore(){
         TreeMap treemap = new TreeMap();
+        Random r = new Random();
+
+        treemap.put(1, r.nextInt(10));
+        treemap.put(2, r.nextInt(10));
+        treemap.put(3, r.nextInt(10));
+        treemap.put(4, r.nextInt(10));
+        treemap.put(5, r.nextInt(10));
+
+        System.out.printf("%s\n", treemap);
+
+        NavigableMap a = treemap.descendingMap();
+
+        System.out.printf("%s\n", a);
     }
 }
